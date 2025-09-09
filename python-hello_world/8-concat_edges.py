233820422ee71921ca390612399e1cc4edb65983
Alexpena76 <alexpenasilva@gmail.com>
2025-09-09 14:25:16 -0500

    Giving this a try

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, high-level, general-purpose programming language"
+str = "Python is an interpreted, high-level, " \
+"general-purpose programming language"
 str = str[39:67] + str[107:112] + str[0:6]
 print(str)
