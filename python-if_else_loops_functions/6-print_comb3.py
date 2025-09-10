ca45645a0d887da3e8116d5c2e5c9f3a4957f23b
Alexpena76 <alexpenasilva@gmail.com>
2025-09-10 16:15:57 -0500

    Corrected indentation for the last line

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for i in range(10):
     for j in range(i + 1, 10):
         print("{:01d}{:01d}".format(i, j),
-                end=", " if not (i == 8 and j == 9) else "\n")
+              end=", " if not (i == 8 and j == 9) else "\n")
