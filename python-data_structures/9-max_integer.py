93c405825e620f7254077455571d0581ccae9029
Alexpena76 <alexpenasilva@gmail.com>
2025-09-16 10:04:49 -0500

    Q9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    
+    max_value = my_list[0]
+    for integer in my_list[1:]:
+        if integer > max_value:
+            max_value = integer
+
+    return max_value
