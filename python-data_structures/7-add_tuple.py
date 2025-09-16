eb0e0de59f14a7a06aac0a403a0987a6993f7552
Alexpena76 <alexpenasilva@gmail.com>
2025-09-16 09:30:26 -0500

    Q7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -7,7 +7,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a1, a2 = tuple_a[0], 0
     else:
         a1, a2 = 0, 0
-    
+
     # Handle tuple_b: get first two elements or use 0 for missing ones
     if len(tuple_b) >= 2:
         b1, b2 = tuple_b[0], tuple_b[1]
@@ -15,6 +15,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b1, b2 = tuple_b[0], 0
     else:
         b1, b2 = 0, 0
-    
+
     # Return tuple with addition of corresponding elements
     return (a1 + b1, a2 + b2)
