4f717b8561ede29fd57396dd129e3b243e5f24c6
Alexpena76 <alexpenasilva@gmail.com>
2025-09-16 09:17:17 -0500

    Q6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, integer in enumerate(row):
+            if i == len(row) - 1:
+                print("{:d}".format(integer))
+            else:
+                print("{:d}".format(integer), end=" ")
+        if not row:
+            print()
