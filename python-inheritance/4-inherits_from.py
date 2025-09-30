5a00981b05c8550451b0faaa0b89ae2e2c86be48
Alexpena76 <alexpenasilva@gmail.com>
2025-09-30 15:11:14 -0500

    Removed whitespace

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,11 +5,11 @@
 def inherits_from(obj, a_class):
     """
     Returns True if obj is an instance of a class that inherited from a_class
-    
+
     Args:
         obj: object to check
         a_class: class to compare against
-    
+
     Returns:
         True if obj's class inherited from a_class, False otherwise
         (does not return True if obj is exactly an instance of a_class)
