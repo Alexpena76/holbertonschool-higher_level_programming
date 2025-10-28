69cf4af277710be50bfaa10ae59c28b94c357b9e
Alexpena76 <alexpenasilva@gmail.com>
2025-10-28 11:15:03 -0500

    Removed the weird header

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,2 @@
-  GNU nano 6.2                                1-create_database_if_missing.sql *
 -- Creates the database hbtn_0c_0 if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
