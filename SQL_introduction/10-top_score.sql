fcc86b7d3813cd3dbf149795a0a807eb66133492
Alexpena76 <alexpenasilva@gmail.com>
2025-10-28 12:34:05 -0500

    Q10

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- Script that lists all records of the table second_table
+-- Results display the score and the name (in this order)
+-- Records are ordered by score (top first)
+
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
