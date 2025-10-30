cfb5c9b751e42ca472d8ff7dc888a92cfaa249c2
Alexpena76 <alexpenasilva@gmail.com>
2025-10-30 11:37:44 -0500

    Added comments to the beginning of the script

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,10 @@
+-- Script: 14-my_genres.sql
+-- Purpose: List all genres of the show "Dexter" from the database hbtn_0d_tvshows
+-- Requirements:
+--   - Display only genre names
+--   - Sort alphabetically
+--   - Use a single SELECT statement
+--   - tv_shows table contains only one row where title = 'Dexter'
 SELECT tv_genres.name
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
